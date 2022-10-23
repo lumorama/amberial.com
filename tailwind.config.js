131932e06737f32b6412572a7e9fba4d19fa25f7
Jay <jay.webp@gmail.com>
2022-10-24 10:47:21 +1100

    Updated TailwindCSS to 3.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
