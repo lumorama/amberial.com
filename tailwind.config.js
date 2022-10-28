301c646881e1c1b285d76e6f80dbb31563cc4080
Jay <jay.webp@gmail.com>
2022-10-28 12:16:16 +1100

    Installed Tailwind cli 1.3, added README.md

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html"],
+  content: [
+    './src/**/*.{html,js}',
+    './index.html',
+  ],
   theme: {
     extend: {
       animation: {
