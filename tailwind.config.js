70b30ee7d8ae26bb305efeb0a471826b7514125a
Jay <jay.webp@gmail.com>
2022-10-30 12:28:33 +1100

    Revert "Installed Tailwind cli 1.3, added README.md"
    This reverts commit 301c646881e1c1b285d76e6f80dbb31563cc4080.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
-/* @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{html,js}',
-    './index.html',
-  ],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       animation: {
