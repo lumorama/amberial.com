747439cda71402a47854e9b8fdfc211d67691a8d
Jay <jay.webp@gmail.com>
2022-10-31 15:25:20 +1100

    Replaced Twitter CTA button, created style.css for Twitter button styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,8 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        rotate: 'rotate 0.3s linear infinite',
-        ball: 'ball 0.75s ease-in',
       },
       keyframes: {
-        ball: {
-          '0%': { transform: 'translateX(85%)' },
-          '10%': { transform: 'translateX(80%) translateY(-30%)' },
-          '35%': { transform: 'translateX(0%) translateY(-30%)' },
-          '40%': { transform: 'translateX(-5%)' },
-          '45%': { transform: 'translateX(0%) translateY(30%)' },
-          '85%': { transform: 'translateX(80%) translateY(30%)' },
-          '100%': { transform: 'translateX(85%)' },
-        },
-        rotate: {
-          '0%': { transform: 'rotate(360deg)' },
-          '100%': { transform: 'rotate(0deg)' }
-        },
       }
     },
   },
