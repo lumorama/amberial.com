6eea3a522a72d6a481162b46edd419c271f14952
Jay <jay.webp@gmail.com>
2022-11-11 17:57:52 +1100

    Add pagination to featured levels display

--- fetch.js
+++ fetch.js
@@ -19,10 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const container = document.querySelector('#fetch');
 
-    const getData = () => {
-        getHttpRequest('GET', 'https://api.mod.io/v1/games/2816/mods?_offset=0&tags-in=Featured&api_key=a98e747e59768daf002bcb5aebcfb1fe' 
+    const getData = (offset, limit) => {
+        getHttpRequest('GET', `https://api.mod.io/v1/games/2816/mods?_offset=${offset}&_limit=${limit}&tags-in=Featured&api_key=a98e747e59768daf002bcb5aebcfb1fe` 
         ).then( response => {
                 displayData(response);
+                // Set results total for pagination
+                total = response.result_total;
             }
         ).catch ( error => {
                 container.textContent = 'ERROR: ' + error.data;
@@ -55,7 +57,50 @@ document.addEventListener('DOMContentLoaded', () => {
             thumbnail.src = value;
             parent.appendChild(thumbnail);
         }
+
     }
     
-    getData();
+    // Paginate results: 7 per page
+    // ?_offset=30&_limit=5 - Will retrieve 5 results after ignoring the first 30 (31 - 35)
+    // result_total - total number of results found
+    // to get 7 featured per page,
+    // ?_offset=0&_limit=7
+
+    // have a go forward and go back button (for pages)
+    // go forward: offset + 7, go back: offset - 7
+    // 0 <= offset, offset < results_total
+
+    let currentOffset = 0;
+    let limit = 7;
+    let total = 0;
+
+    document.querySelector('#previous-page').addEventListener('click', () => {
+        clearChildren(container);
+        paginate(-1);
+        getData(currentOffset, limit);
+    });
+
+    document.querySelector('#next-page').addEventListener('click', () => {
+        clearChildren(container);
+        paginate(1);
+        getData(currentOffset, limit);
+    });
+
+    function paginate(multiplier) {
+        if (currentOffset + (limit * multiplier) > total) {
+            return currentOffset = total - (total % limit);
+        } else if (currentOffset + (limit * multiplier) <= 0) {
+            return currentOffset = 0;
+        } else {
+            return currentOffset += (limit * multiplier);
+        }
+    }
+
+    function clearChildren(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+
+    getData(currentOffset, limit);
 })
