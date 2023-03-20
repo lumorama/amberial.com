17caa532ee2a4db9d2dcede655609c1c6a40fbb7
oddgoo <oddgoo@gmail.com>
2023-03-20 12:04:35 +1100

    -oddgoo interview updated

---
+++ interview/_next/static/Upj4LqtFl50Oiu0qqnQhg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e669712b190293a3.css","static/chunks/pages/index-ea0cc32968ff67dd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
