cdf9b01b40c0818501bcc0bc04eb4e47f84ef766
oddgoo <oddgoo@gmail.com>
2023-03-22 10:09:38 +1100

    -Interview: full replace

---
+++ interview/_next/static/hNTLmBwPOlkmN7tAbkt6O/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e669712b190293a3.css","static/chunks/pages/index-c503cd60a0fd4717.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
